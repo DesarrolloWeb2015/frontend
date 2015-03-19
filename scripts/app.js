fd86a29a3b535e9700f4eac2d85efa9c5429fc49
Pablo Máximo Álvarez <pablo.a.maximo@gmail.com>
2015-03-19 02:38:24 +0100

    Initial commit, home page html and css

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+(function (document) {
+
+	'use strict';
+	/**
+	 * @ngdoc overview
+	 * @name PolymerBricks
+	 * @description
+	 * # PolymerBricks
+	 *
+	 * Main module of the application.
+	*/
+	angular
+	.module('TecnoCrown', [
+		'ngRoute'
+	])
+	.config(function ($locationProvider, $routeProvider) {
+
+		$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'HomeCtrl'
+
+	});
+
+})(wrap(document));
