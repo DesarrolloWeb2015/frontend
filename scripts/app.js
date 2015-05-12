8e7eb1904bf6abf2f0dfa7304583a44bba5683b0
Miguel Ortega <miguel_ortega5@hotmail.com>
2015-05-13 00:07:36 +0200

    Añadido algo de logica y lenguages antes del merge de projecto

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,55 @@
-angular.module("TecnoCrown", []);
+var app = angular.module("TecnoCrown", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+
+  $routeProvider
+  /* Español */
+    .when('/project/:projectId', {
+    templateUrl: 'project.html',
+    controller: 'globalCtrl'
+  });
+});
+
+app.controller('globalCtrl',function($scope, $http){
+  $scope.language = $scope.language || {};
+
+  $http.get('lang/en_en.json').success(function (data,status) {
+    $scope.language = data;
+  });
+
+  $scope.changelang = function (language) {
+    var file;
+    file = language + '_' + language + '.json';
+    $http.get('lang/' + file).success(function (data,status) {
+      $scope.language = data;
+    });
+  };
+});
+
+app.service('$backend',function ($scope, $http) {
+  this.login = function (user, password, callback, errorCallback) {
+
+  };
+
+  this.logout = function (user, callback, errorCallback) {
+
+  };
+
+  this.createProject = function (user,name, tille, time, money ,callback, errorCallback) {
+
+  };
+
+  this.myProject = function (user, callback, errorCallback) {
+
+  };
+
+  this.deleteMyProject = function (user,projectId, callback, errorCallback) {
+
+  };
+
+  this.addCrownfounding = function (user, projectId, money, callback, errorCallback) {
+
+  };
+
+
+});
--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,8 @@
 angular.module('TecnoCrown')
   .controller('HomeCtrl', function($scope) {
     $scope.crowfounding = [
-    {name:'TecnoCrown',link:'www.tecnocrown.html',description:'Projecto para la construccion de un portal web',current:'15.000',remain:'21',needed:'30.000',img:'images/proy1.png'},
-    {name:'Polymer',link:'www.prueba.html',description:'Desarrollo de web component mediante tecnología Polymer',current:'15.000',remain:'21',needed:'30.000',img:'images/imagen2.png'},
-    {name:'AngularJs',link:'www.prueba.html',description:'Desarrollo de librerias para representación de gráficos',current:'15.000',remain:'21',needed:'30.000',img:'images/angularjs.jpg'}
+    {name:'TecnoCrown',link:'www.tecnocrown.html',description:'Projecto para la construccion de un portal web',current:'15.000',remain:'21',needed:'30.000',img:'images/proy1.png',id:'1'},
+    {name:'Polymer',link:'www.prueba.html',description:'Desarrollo de web component mediante tecnología Polymer',current:'15.000',remain:'21',needed:'30.000',img:'images/imagen2.png', id: '2'},
+    {name:'AngularJs',link:'www.prueba.html',description:'Desarrollo de librerias para representación de gráficos',current:'15.000',remain:'21',needed:'30.000',img:'images/angularjs.jpg', id:'3'}
   ];
 });
