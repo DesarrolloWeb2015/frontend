0882f169da75c564ecc8274dc3492a140da5d90e
Miguel Ortega <miguel_ortega5@hotmail.com>
2015-06-08 23:21:22 +0200

    Directiva de leer ficheros

--- scripts/app.js
+++ scripts/app.js
@@ -481,3 +481,22 @@ tecnocrownApp.controller('detailCtrl',['$scope','api','$cookie',function($scope,
   }
   $scope.load()
 }]);
+
+tecnocrownApp.directive("fileread", [function () {
+  return {
+    scope: {
+      fileread: "="
+    },
+    link: function (scope, element, attributes) {
+      element.bind("change", function (changeEvent) {
+        var reader = new FileReader();
+        reader.onload = function (loadEvent) {
+          scope.$apply(function () {
+            scope.fileread = loadEvent.target.result;
+          });
+        }
+        reader.readAsDataURL(changeEvent.target.files[0]);
+      });
+    }
+  }
+}]);
